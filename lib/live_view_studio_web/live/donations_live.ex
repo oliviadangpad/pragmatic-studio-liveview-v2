4f90e6f474d589d452b37557699cd150d1f9b9a5
Olivia Dang <78009350+oliviadangpad@users.noreply.github.com>
2023-03-13 14:37:11 +1100

    13. Pagination

--- lib/live_view_studio_web/live/donations_live.ex
+++ lib/live_view_studio_web/live/donations_live.ex
@@ -11,9 +11,14 @@ defmodule LiveViewStudioWeb.DonationsLive do
     sort_by = valid_sort_by(params)
     sort_order = valid_sort_order(params)
 
+    page = (params["page"] || "1") |> String.to_integer()
+    per_page = (params["per_page"] || "5") |> String.to_integer()
+
     options = %{
       sort_by: sort_by,
-      sort_order: sort_order
+      sort_order: sort_order,
+      page: page,
+      per_page: per_page
     }
 
     donations = Donations.list_donations(options)
@@ -30,22 +35,28 @@ defmodule LiveViewStudioWeb.DonationsLive do
 
   defp valid_sort_by(_params), do: :id
 
-  defp %{"sort_order" => sort_order}
+  defp valid_sort_order(%{"sort_order" => sort_order})
        when sort_order in ~w(asc desc) do
     String.to_atom(sort_order)
   end
 
-  defp valid_sort_order(_params), do: :desc
+  defp valid_sort_order(_params), do: :asc
 
   attr(:sort_by, :atom, required: true)
   attr(:options, :map, required: true)
   slot(:inner_block, required: true)
 
   def sort_link(assigns) do
+    params = %{
+      assigns.options
+      | sort_by: assigns.sort_by,
+        sort_order: next_sort_order(assigns.options.sort_order)
+    }
+
+    assigns = assign(assigns, params: params)
+
     ~H"""
-    <.link patch={
-      ~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"
-    }>
+    <.link patch={~p"/donations?#{params}"}>
       <%= render_slot(@inner_block) %>
       <%= sort_indicator(@sort_by, @options) %>
     </.link>
@@ -68,4 +79,10 @@ defmodule LiveViewStudioWeb.DonationsLive do
   end
 
   defp sort_indicator(_, _), do: ""
+
+  def handle_event("select-per-page", %{"per-page" => per_page}, socket) do
+    params = %{socket.assigns.options | per_page: per_page}
+    socket = push_patch(socket, to: ~p"/donations?#{params}")
+    {:noreply, socket}
+  end
 end
